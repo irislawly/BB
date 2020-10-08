628294813fd964f1fd4a5861628b6b5df6dcd3ce
Iris Law <irislawly@gmail.com>
2020-10-08 16:56:37 -0700

    added login, connect fiebase

---
+++ app/src/main/java/com/bcit/bb/bookings.java
@@ -0,0 +1,15 @@
+package com.bcit.bb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bookings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookings);
+
+    }
+}
