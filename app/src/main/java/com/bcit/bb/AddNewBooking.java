d241ff676f0bd5dc0b08975c9232534b4291dbed
yiwen950725 <tiffany-gu@hotmail.com>
2020-10-08 23:46:47 -0700

    added edit and add new bookings pages

---
+++ app/src/main/java/com/bcit/bb/AddNewBooking.java
@@ -0,0 +1,18 @@
+package com.bcit.bb;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddNewBooking extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_booking);
+
+    }
+
+
+}
