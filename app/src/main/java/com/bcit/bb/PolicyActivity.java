c6833b65fdc51f051a9bac6d9298eb8961693cbd
Billy Nguyen <bquynhnguyen209@gmail.com>
2020-10-15 21:52:03 -0700

    changed app's icon

---
+++ app/src/main/java/com/bcit/bb/PolicyActivity.java
@@ -0,0 +1,14 @@
+package com.bcit.bb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_policy);
+    }
+}
