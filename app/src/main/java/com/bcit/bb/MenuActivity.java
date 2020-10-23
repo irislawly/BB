8efcd19e89cdab745608ceeeaa2d965348afe03d
Iris Law <irislawly@gmail.com>
2020-10-23 14:07:59 -0700

    added signout button

--- app/src/main/java/com/bcit/bb/MenuActivity.java
+++ app/src/main/java/com/bcit/bb/MenuActivity.java
@@ -3,10 +3,14 @@ package com.bcit.bb;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,4 +36,11 @@ public class MenuActivity extends AppCompatActivity {
         Intent intent = new Intent(this, YourAccountActivity.class);
         startActivity(intent);
     }*/
+
+    public void onGymSignoutClick(View view) {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
 }
