90fb74ffba52d5059e26652631f7abdb7b94923c
Iris Law <irislawly@gmail.com>
2020-10-23 20:00:12 -0700

    fixed up add new booking, added signout button

---
+++ app/src/main/java/com/bcit/bb/uiFeatures/DatePickerFragment.java
@@ -0,0 +1,41 @@
+package com.bcit.bb.uiFeatures;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+import static com.firebase.ui.auth.AuthUI.getApplicationContext;
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(),  (DatePickerDialog.OnDateSetListener)getActivity(), year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        String date = month + " " + day + ", " + year;
+
+    }
+}
