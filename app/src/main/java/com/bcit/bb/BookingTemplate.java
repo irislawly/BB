aee09c3b9f5686170559982acc0f32be35b49e52
Iris Law <irislawly@gmail.com>
2020-10-30 00:00:36 -0700

    did edit button and reutnr to menu page

--- app/src/main/java/com/bcit/bb/BookingTemplate.java
+++ app/src/main/java/com/bcit/bb/BookingTemplate.java
@@ -6,6 +6,7 @@ public class BookingTemplate implements Serializable {
     private String equipment;
     private String timeslot;
     private String date;
+    private String id;
 
     public String getEquipment() {
         return equipment;
@@ -18,10 +19,14 @@ public class BookingTemplate implements Serializable {
     public String getDate() {
         return date;
     }
+    public String getId() {
+        return id;
+    }
 
-    public BookingTemplate(String equipment, String timeslot, String date) {
+    public BookingTemplate(String equipment, String timeslot, String date, String id) {
         this.equipment = equipment;
         this.timeslot = timeslot;
         this.date = date;
+        this.id = id;
     }
 }
