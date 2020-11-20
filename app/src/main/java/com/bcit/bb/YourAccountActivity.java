67b5b0738404b03249f38d8f90df53eaff76e1d9
Billy Nguyen <bquynhnguyen209@gmail.com>
2020-11-20 10:05:00 -0800

    implemented dialogs

--- app/src/main/java/com/bcit/bb/YourAccountActivity.java
+++ app/src/main/java/com/bcit/bb/YourAccountActivity.java
@@ -4,12 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
-<<<<<<< HEAD
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
