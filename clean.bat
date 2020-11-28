fbb91b27c83afc82cc898654bcf25b5035f49ae3
Billy Nguyen <bquynhnguyen209@gmail.com>
2020-11-27 22:59:17 -0800

    fixed header for policyActivity

---
+++ clean.bat
@@ -0,0 +1,10 @@
+del ./*.iml
+rmdir /Q /s ./.gradle
+del /F /Q ./local.properties
+rmdir /Q /s .idea/libraries
+del ./.idea/modules.xml
+del ./.idea/workspace.xml
+del ./.DS_Store
+rmdir /Q /s ./app/build
+rmdir /Q /s ./captures
+del ./.externalNativeBuild
